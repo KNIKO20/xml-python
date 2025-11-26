f66fc5c247d0c52fa0ab08b7329e858a36ea5af0
Michael <meldn.work01@gmail.com>
2025-11-26 14:23:36 +0100

    mas informacion y contenido sobre como meter, sacar infor del XML

--- prestamo.py
+++ prestamo.py
@@ -1,30 +1,27 @@
-# 2 GESTION LIBROS:
-                # AÑADIR
-                # MOSTRAR
-                # ELIMINAR
-                # BUSQUEDA
             # 3 GESTION PRESTAMOS:
                 # PRESTAMO: 
                 # DEVOLUCION:
                 
                 
-#  Para obtener el elemento raíz del documento, se utiliza el método getroot():
+# 1. IMPORTAR LIBRERIAS
 
-# raiz = doc.getroot()
-# print(raiz.tag)               
-     
-#ORDEN DE EJECUCION:
-    #Primero eneseñar un"menu" donde tenga: preguntará al usuario si quiere añadir, mostrar información
-        #[Cualquier acción que implique una búsqueda deberá devolver todas las opciones posibles, imprimiendo en pantalla estas mismas y dando a elegir que elemento debe seleccionar]
-    # o eliminar a un libro del catálogo.
-    # Segundo, al elegir la opcion ejecutar la funcion respectiva de esa opcion
-    # Tercero, los cambios que haga deben guardarse en el archivo XML elegido.
-    # Cuarto, al sacar informacion, hacer que los datos salgan por la consola en un formato adecuado, y no con el formato de etiquetas.
-    
-# Para las funciones respectivas a cada opcion del menu, necesito importar las librerias necesarias para ello.
-# Ademas, se debe establecer el formato con el que saldran los datos por la terminal. {ID: 001 | Título: El Quijote | Autor: Cervantes | Disponible: Sí/No}
-# Búsqueda y selección
+# 2. FORMATO
 
-# Si el usuario busca por título o autor, listar todas las coincidencias.
+# 3. BUSQUEDA Y SELECCION
 
-# Permitir elegir el libro exacto por ID.               
+# 4. GUARDAR CAMBIOS
+
+import xml.etree.ElementTree as ET
+tree = ET.parse('AQUI VA LA RUTA DEL DOCUMENTO XML')
+root = tree.getroot()
+for elem in root:
+    print(elem.tag, elem.text)# abrir y leer elementos del XML
+
+def prestarLibro():
+    return
+
+def devolucionLibro():
+    return
+
+def saveInfo(): #El fichero XML deberá guardar la estructura definida en el XML Schema.
+    return
