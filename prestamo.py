24b5910db1bed7e7374eb1dea512b987457efd27
Michael <meldn.work01@gmail.com>
2025-11-26 13:09:58 +0100

    cambios michael: creacion de clase libros.py y redaccion de tareas de prestamo.py (26/11/205)

--- prestamo.py
+++ prestamo.py
@@ -1,4 +1,30 @@
-# Preguntar si hay prestamos o no.
-            # Bucle prestamo:
-                # id (creado por nosotro como atributo)
-                # usuario (id del usuario como atributo)
+# 2 GESTION LIBROS:
+                # AÑADIR
+                # MOSTRAR
+                # ELIMINAR
+                # BUSQUEDA
+            # 3 GESTION PRESTAMOS:
+                # PRESTAMO: 
+                # DEVOLUCION:
+                
+                
+#  Para obtener el elemento raíz del documento, se utiliza el método getroot():
+
+# raiz = doc.getroot()
+# print(raiz.tag)               
+     
+#ORDEN DE EJECUCION:
+    #Primero eneseñar un"menu" donde tenga: preguntará al usuario si quiere añadir, mostrar información
+        #[Cualquier acción que implique una búsqueda deberá devolver todas las opciones posibles, imprimiendo en pantalla estas mismas y dando a elegir que elemento debe seleccionar]
+    # o eliminar a un libro del catálogo.
+    # Segundo, al elegir la opcion ejecutar la funcion respectiva de esa opcion
+    # Tercero, los cambios que haga deben guardarse en el archivo XML elegido.
+    # Cuarto, al sacar informacion, hacer que los datos salgan por la consola en un formato adecuado, y no con el formato de etiquetas.
+    
+# Para las funciones respectivas a cada opcion del menu, necesito importar las librerias necesarias para ello.
+# Ademas, se debe establecer el formato con el que saldran los datos por la terminal. {ID: 001 | Título: El Quijote | Autor: Cervantes | Disponible: Sí/No}
+# Búsqueda y selección
+
+# Si el usuario busca por título o autor, listar todas las coincidencias.
+
+# Permitir elegir el libro exacto por ID.               
