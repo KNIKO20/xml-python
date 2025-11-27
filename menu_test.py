f9cd502559cd412fb9b42f6abf971ce6e4a01369
Kevin VF <knvfons20@gmail.com>
2025-11-27 11:15:09 +0100

    idk

--- menu_test.py
+++ menu_test.py
@@ -1,7 +1,4 @@
 from menu import menu
-import os
-import msvcrt
-from sys import platform
 
 
 title1 = """                                                                                                              
@@ -13,41 +10,13 @@ title1 = """
 mainMenu = menu(title=title1)
 
 mainMenu.showSubMenu()
-mainMenu.selectSubMenu()
+mainMenu.selectOption(int(input("Selecciona una opcion: ")))
+
+
+mainMenu.showWindowsMenu()
 
 
 
-#SOLO FUNCIONA EN WINDOWS
-if (platform == "win32"):
-    opciones = mainMenu.showSubMenuOptions()
-    seleccion = 0
 
-    while True:
-         # Mostrar menú
-        for i, opcion in enumerate(opciones):
-            if i == seleccion:
-                print(f"> {opcion}")  # resaltado
-            else:
-                print(f"  {opcion}")
 
-         # Leer tecla
-        tecla = msvcrt.getch()
-
-        if tecla == b'H':  # Flecha arriba
-            seleccion = (seleccion - 1) % len(opciones)
-        elif tecla == b'P':  # Flecha abajo
-            seleccion = (seleccion + 1) % len(opciones)
-        elif tecla == b'\r':  # Enter
-            print(f"Has elegido: {opciones[seleccion]}")
-            mainMenu.selectSubMenu(seleccion)
-            if opciones[seleccion] == "Salir":
-                break
-            break
-
-         # Limpiar pantalla para redibujar
-
-        os.system("cls")
-else:
-    mainMenu.show()
-    mainMenu.selectSubMenu()
 
